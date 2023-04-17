391409b8a4e70cc1590fa06440dfad38938390a1
YuchiYen <yuchiyen@hotmail.com>
2023-04-16 22:57:50 -0700

    finished assignment

--- daos/items.js
+++ daos/items.js
@@ -9,16 +9,35 @@ module.exports.getAll = () => {
   return itemsModel.items;
 }
 
-module.exports.getById = (itemId) => {
-  // TODO: complete
+module.exports.getById = (itemId) => {  
+    // TODO: complete  
+    return itemsModel.items.find( i => i.id === itemId);    
 }
 
 module.exports.deleteById = async (itemId) => {
     // TODO: complete
+
+    let index = itemsModel.items.findIndex(item => item.id === itemId);
+    if (index >= 0) {
+      itemsModel.items.splice(index, 1);
+      return true;
+    }
+    return false;
 }
 
 module.exports.updateById = async (itemId, newObj) => {
-    // TODO: complete
+  // TODO: complete
+  let itemToUpdate = itemsModel.items.find(i => i.id === itemId);
+
+  if (itemToUpdate) {
+    for (let propertyName in newObj) {
+      if (propertyName === "id")
+        continue;
+      itemToUpdate[propertyName] = newObj[propertyName];
+    }
+    return true
+  }
+  return false;
 }
 
 module.exports.create = async (item) => {
